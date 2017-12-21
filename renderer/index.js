437bc976fe5997d3434ed47495e339e2e37ad31d
= <=>
2017-12-21 22:11:11 +0100

    snake AI

--- renderer/index.js
+++ renderer/index.js
@@ -1,6 +1,5 @@
 const ipc = require('electron').ipcRenderer
 
-
 ipc.on('index', (event, message) => {
     console.log(message); // logs out "Hello second window!"
 })
@@ -8,3 +7,5 @@ ipc.on('index', (event, message) => {
 function send_message(receiving_window,message){
 	ipc.send('message', {receiver:receiving_window,message:message});
 }
+
+
